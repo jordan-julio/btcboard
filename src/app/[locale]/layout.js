b2a5ee69378a56b56b2887989839feca748af3a7
jordan-julio <jordan.julio.jap@gmail.com>
2025-08-13 13:00:40 +0700

    add analytics

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -3,6 +3,7 @@ import {Geist, Geist_Mono} from "next/font/google";
 import "../globals.css";
 import {NextIntlClientProvider} from "next-intl";
 import {getMessages} from "next-intl/server";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({variable: "--font-geist-sans", subsets: ["latin"]});
 const geistMono = Geist_Mono({variable: "--font-geist-mono", subsets: ["latin"]});
@@ -24,6 +25,7 @@ export default async function LocaleLayout({children, params}) {
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
+        <Analytics />
       </body>
     </html>
   );
